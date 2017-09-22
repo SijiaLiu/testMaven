927fa4f0567c4df63f43a25191d66295b3216ffe
liusijia <79891847@qq.com>
2017-09-22 13:13:10 +0800

    test github

---
+++ src/main/java/com/lsj/controller/test.java
@@ -0,0 +1,10 @@
+package com.lsj.controller;
+
+/**
+ * Created by liusijia on 2017/9/22.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
